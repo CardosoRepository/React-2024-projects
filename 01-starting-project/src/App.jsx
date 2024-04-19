4df6353c28ee6cccc21619586e0f529a9499a204
Gabriel Cardoso Barbosa <gabrielcard0s0@hotmail.com>
2024-04-19 16:06:30 -0300

    Spliting components in multiple files

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,37 +1,8 @@
-import reactCoreConceptsImg from "./assets/react-core-concepts.png";
+import { Header } from "./components/Header";
+import { CoreConcept } from "./components/CoreConcept";
 import { CORE_CONCEPTS } from "./data";
 
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-
-function genRandomInt(max) {
-    return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-    const description = reactDescriptions[genRandomInt(2)];
-    return (
-        <header>
-            <img src={reactCoreConceptsImg} alt="Stylized atom" />
-            <h1>React Essentials</h1>
-            <p>
-                {description} React concepts you will need for almost any app
-                you are going to build!
-            </p>
-        </header>
-    );
-}
-
-function CoreConcept({image, title, description}) {
-    return (
-        <li>
-            <img src={image} alt={title} />
-            <h3>{title}</h3>
-            <p>{description}</p>
-        </li>
-    );
-}
-
-function App() {
+export function App() {
     return (
         <div>
             <Header />
@@ -43,31 +14,9 @@ function App() {
                         <CoreConcept {...CORE_CONCEPTS[1]} />
                         <CoreConcept {...CORE_CONCEPTS[2]} />
                         <CoreConcept {...CORE_CONCEPTS[3]} />
-                        {/* <CoreConcept
-                            title={CORE_CONCEPTS[0].title}
-                            description={CORE_CONCEPTS[0].description}
-                            image={CORE_CONCEPTS[0].image}
-                        />
-                        <CoreConcept
-                            title={CORE_CONCEPTS[1].title}
-                            description={CORE_CONCEPTS[1].description}
-                            image={CORE_CONCEPTS[1].image}
-                        />
-                        <CoreConcept
-                            title={CORE_CONCEPTS[2].title}
-                            description={CORE_CONCEPTS[2].description}
-                            image={CORE_CONCEPTS[2].image}
-                        />
-                        <CoreConcept
-                            title={CORE_CONCEPTS[3].title}
-                            description={CORE_CONCEPTS[3].description}
-                            image={CORE_CONCEPTS[3].image}
-                        /> */}
                     </ul>
                 </section>
             </main>
         </div>
     );
 }
-
-export default App;
