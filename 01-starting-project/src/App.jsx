3e6e9c68e3f7e343325f70771e4085dab9b3d081
Gabriel Cardoso Barbosa <gabrielcard0s0@hotmail.com>
2024-04-19 15:38:31 -0300

    Reutilizing a component with props

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,4 +1,5 @@
 import reactCoreConceptsImg from "./assets/react-core-concepts.png"
+import componentsImg from "./assets/components.png";
 
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
@@ -19,12 +20,30 @@ function Header() {
         </header>
     );
 }
+
+function CoreConcept(props) {
+    return (
+        <li>
+            <img src={props.img} alt={props.title} />
+            <h3>{props.title}</h3>
+            <p>{props.description}</p>
+        </li>
+    )
+}
+
 function App() {
     return (
         <div>
             <Header />
             <main>
-                <h2>Time to get started!</h2>
+                <section id="core-concepts">
+                    <h2>Time to get started!</h2>
+                    <ul>
+                        <CoreConcept title="Components" description="The core UI building block" img={componentsImg} />
+                        <CoreConcept title="Props" description="The core UI building block" img={componentsImg} />
+                        <CoreConcept title="Components" description="The core UI building block" />
+                    </ul>
+                </section>
             </main>
         </div>
     );
