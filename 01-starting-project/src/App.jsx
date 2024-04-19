a6f2668173700524ce2bd8e8b7e792682f2a5a11
Gabriel Cardoso Barbosa <gabrielcard0s0@hotmail.com>
2024-04-19 15:50:22 -0300

    Writing less code for CoreConcept function

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,5 +1,5 @@
-import reactCoreConceptsImg from "./assets/react-core-concepts.png"
-import componentsImg from "./assets/components.png";
+import reactCoreConceptsImg from "./assets/react-core-concepts.png";
+import { CORE_CONCEPTS } from "./data";
 
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
@@ -14,21 +14,21 @@ function Header() {
             <img src={reactCoreConceptsImg} alt="Stylized atom" />
             <h1>React Essentials</h1>
             <p>
-                {description} React concepts you will need for almost any app you are
-                going to build!
+                {description} React concepts you will need for almost any app
+                you are going to build!
             </p>
         </header>
     );
 }
 
-function CoreConcept(props) {
+function CoreConcept({image, title, description}) {
     return (
         <li>
-            <img src={props.img} alt={props.title} />
-            <h3>{props.title}</h3>
-            <p>{props.description}</p>
+            <img src={image} alt={title} />
+            <h3>{title}</h3>
+            <p>{description}</p>
         </li>
-    )
+    );
 }
 
 function App() {
@@ -39,9 +39,30 @@ function App() {
                 <section id="core-concepts">
                     <h2>Time to get started!</h2>
                     <ul>
-                        <CoreConcept title="Components" description="The core UI building block" img={componentsImg} />
-                        <CoreConcept title="Props" description="The core UI building block" img={componentsImg} />
-                        <CoreConcept title="Components" description="The core UI building block" />
+                        <CoreConcept {...CORE_CONCEPTS[0]} />
+                        <CoreConcept {...CORE_CONCEPTS[1]} />
+                        <CoreConcept {...CORE_CONCEPTS[2]} />
+                        <CoreConcept {...CORE_CONCEPTS[3]} />
+                        {/* <CoreConcept
+                            title={CORE_CONCEPTS[0].title}
+                            description={CORE_CONCEPTS[0].description}
+                            image={CORE_CONCEPTS[0].image}
+                        />
+                        <CoreConcept
+                            title={CORE_CONCEPTS[1].title}
+                            description={CORE_CONCEPTS[1].description}
+                            image={CORE_CONCEPTS[1].image}
+                        />
+                        <CoreConcept
+                            title={CORE_CONCEPTS[2].title}
+                            description={CORE_CONCEPTS[2].description}
+                            image={CORE_CONCEPTS[2].image}
+                        />
+                        <CoreConcept
+                            title={CORE_CONCEPTS[3].title}
+                            description={CORE_CONCEPTS[3].description}
+                            image={CORE_CONCEPTS[3].image}
+                        /> */}
                     </ul>
                 </section>
             </main>
