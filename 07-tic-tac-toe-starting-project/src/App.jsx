005d041833ae07be19406ebf30185c632c633207
Gabriel Cardoso Barbosa <gabrielcard0s0@hotmail.com>
2024-04-22 17:49:38 -0300

    Deriving active player data

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -3,24 +3,29 @@ import { GameBoard } from "./components/GameBoard";
 import { Player } from "./components/Player";
 import { Log } from "./components/Log";
 
+function deriveActivePlayer(gameTurns) {
+    let currentPlayer = "X";
+
+    if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+        currentPlayer = "O";
+    }
+
+    return currentPlayer;
+}
+
 export function App() {
-    const [activePlayer, setActivePlayer] = useState("X");
     const [gameTurns, setGameTurns] = useState([]);
+    const activePlayer = deriveActivePlayer(gameTurns);
 
     function handleSelectSquare(rowIndex, colIndex) {
-        setActivePlayer((curActivePlayer) =>
-            curActivePlayer === "X" ? "O" : "X"
-        );
         setGameTurns((prevTurns) => {
-            console.log(prevTurns);
-            let currentPlayer = "X";
-
-            if (prevTurns.length > 0 && prevTurns[0].player === "X") {
-                currentPlayer = "O";
-            }
+            let currentPlayer = deriveActivePlayer(prevTurns);
 
             const updatedTurns = [
-                { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+                {
+                    square: { row: rowIndex, col: colIndex },
+                    player: currentPlayer,
+                },
                 ...prevTurns,
             ];
 
