9bc1a7c1ee461622e30c38aa5f482826ef18e5dc
Gabriel Cardoso Barbosa <gabrielcard0s0@hotmail.com>
2024-04-23 16:43:15 -0300

    Using styled components on project

--- 03-styling-react/src/components/AuthInputs.jsx
+++ 03-styling-react/src/components/AuthInputs.jsx
@@ -1,4 +1,14 @@
 import { useState } from "react";
+import { styled } from "styled-components";
+import { Button } from "./Buttons";
+import { CustomInput } from "./Input";
+
+const ControlContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    margin-bottom: 1.5rem;
+`;
 
 export function AuthInputs() {
     const [enteredEmail, setEnteredEmail] = useState("");
@@ -22,35 +32,29 @@ export function AuthInputs() {
 
     return (
         <div id="auth-inputs">
-            <div className="controls">
-                <p className="paragraph">
-                    <label className={`label ${emailNotValid ? "invalid" : ""}`}>Email</label>
-                    <input
-                        type="email"
-                        className={emailNotValid ? "invalid" : undefined}
-                        onChange={(event) =>
-                            handleInputChange("email", event.target.value)
-                        }
-                    />
-                </p>
-                <p>
-                    <label className={`label ${passwordNotValid ? "invalid" : ""}`}>Password</label>
-                    <input
-                        type="password"
-                        className={passwordNotValid ? "invalid" : undefined}
-                        onChange={(event) =>
-                            handleInputChange("password", event.target.value)
-                        }
-                    />
-                </p>
-            </div>
+            <ControlContainer>
+                <CustomInput
+                    label="Email"
+                    invalid={emailNotValid}
+                    type="email"
+                    onChange={(event) =>
+                        handleInputChange("email", event.target.value)
+                    }
+                />
+                <CustomInput
+                    label="Password"
+                    invalid={passwordNotValid}
+                    type="password"
+                    onChange={(event) =>
+                        handleInputChange("password", event.target.value)
+                    }
+                />
+            </ControlContainer>
             <div className="actions">
                 <button type="button" className="text-button">
                     Create a new account
                 </button>
-                <button className="button" onClick={handleLogin}>
-                    Sign In
-                </button>
+                <Button onClick={handleLogin}>Sign In</Button>
             </div>
         </div>
     );
