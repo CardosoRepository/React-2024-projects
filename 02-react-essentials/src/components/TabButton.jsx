79f37a49afcf45eb4c94cd930a7147191ae11e1b
Gabriel Cardoso Barbosa <gabrielcard0s0@hotmail.com>
2024-04-22 23:57:59 -0300

    Getting started with investment calculator

--- 02-react-essentials/src/components/TabButton.jsx
+++
@@ -1,7 +0,0 @@
-export function TabButton({ children, isSelected, ...props }) {
-    return (
-        <li>
-            <button className={isSelected ? "active" : ""} {...props}>{children}</button>
-        </li>
-    );
-}
