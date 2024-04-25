ce2baff403045abb672d93490ed9e82dd07f910d
Gabriel Cardoso Barbosa <gabrielcard0s0@hotmail.com>
2024-04-25 16:14:34 -0300

    Selecting project and highlight on Sidebar

--- 05-project-management/src/App.jsx
+++ 05-project-management/src/App.jsx
@@ -3,12 +3,28 @@ import { NewProject } from "./components/Project/NewProject";
 import { Sidebar } from "./components/Sidebar";
 import { NoProjectSelected } from "./components/project/NoProjectSelected";
 import uuidv4 from "node-uuid";
+import { ProjectDetails } from "./components/project/ProjectDetails";
 
 export function App() {
     const [projectsState, setProjectsState] = useState({
         selectedProjectId: undefined,
-        projects: [],
+        projects: [
+            {
+                description: "Lorem ipsum",
+                dueDate: "2024-04-25",
+                id: "b89d11db-ef26-4af7-aef1-6764b0cef6f4",
+                title: "Project title",
+            },
+            {
+                description: "...Lorem ipsum...",
+                dueDate: "2024-04-20",
+                id: "2131231231231",
+                title: "Second project",
+            },
+        ],
     });
+
+    const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId);
 
     function handleStartAddProject() {
         setProjectsState((prevState) => {
@@ -43,6 +59,15 @@ export function App() {
         });
     }
 
+    function handleSelectProject(id) {
+        setProjectsState((prevState) => {
+            return {
+                ...prevState,
+                selectedProjectId: id,
+            };
+        });
+    }
+
     return (
         <main className="flex flex-col h-screen">
             <div className="my-8"></div>
@@ -50,14 +75,21 @@ export function App() {
                 <Sidebar
                     onStartAddProject={handleStartAddProject}
                     projects={projectsState.projects}
+                    onSelectProject={handleSelectProject}
+                    selectedProjectId={projectsState.selectedProjectId}
                 />
 
                 {projectsState.selectedProjectId === null ? (
-                    <NewProject onAdd={handleAddProject} onCancel={handleCancel} />
-                ) : (
+                    <NewProject
+                        onAdd={handleAddProject}
+                        onCancel={handleCancel}
+                    />
+                ) : projectsState.selectedProjectId === undefined ? (
                     <NoProjectSelected
                         onStartAddProject={handleStartAddProject}
                     />
+                ) : (
+                    <ProjectDetails project={selectedProject} />
                 )}
             </div>
         </main>
