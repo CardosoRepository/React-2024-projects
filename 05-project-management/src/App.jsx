c372401ae4bd84f790c4fe345ead129875f35695
Gabriel Cardoso Barbosa <gabrielcard0s0@hotmail.com>
2024-04-25 13:09:46 -0300

    Selecting new project at main page

--- 05-project-management/src/App.jsx
+++ 05-project-management/src/App.jsx
@@ -1,12 +1,34 @@
+import { useState } from "react";
 import { NewProject } from "./components/Project/NewProject";
 import { Sidebar } from "./components/Sidebar";
+import { NoProjectSelected } from "./components/project/NoProjectSelected";
 
 export function App() {
+    const [projectsState, setProjectsState] = useState({
+        selectedProjectId: undefined,
+        projects: [],
+    });
+
+    function handleStartAddProject() {
+        setProjectsState((prevState) => {
+            return {
+                ...prevState,
+                selectedProjectId: null,
+            };
+        });
+    }
 
     return (
-        <main className="flex h-screen my-8 gap-8">
-            <Sidebar />
-            <NewProject />
+        <main className="flex flex-col h-screen">
+            <div className="my-8"></div>
+            <div className="flex h-full gap-8">
+                <Sidebar onStartAddProject={handleStartAddProject} />
+
+                {projectsState.selectedProjectId === null ? 
+                    <NewProject /> :
+                    <NoProjectSelected onStartAddProject={handleStartAddProject} />
+                }
+            </div>
         </main>
     );
 }
