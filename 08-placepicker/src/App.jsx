abbe60d32aba73260528f8b75e7c635efa307207
Gabriel Cardoso Barbosa <gabrielcard0s0@hotmail.com>
2024-04-28 18:06:25 -0300

    Storing picked places on local storage

--- 08-placepicker/src/App.jsx
+++ 08-placepicker/src/App.jsx
@@ -7,11 +7,17 @@ import DeleteConfirmation from "./components/DeleteConfirmation.jsx";
 import logoImg from "./assets/logo.png";
 import { sortPlacesByDistance } from "./loc.js";
 
+const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+const storedPlaces = storedIds.map((id) =>
+    AVAILABLE_PLACES.find((place) => place.id === id)
+);
+
 export function App() {
+
     const modal = useRef();
     const selectedPlace = useRef();
     const [availablePlaces, setAvailablePlaces] = useState();
-    const [pickedPlaces, setPickedPlaces] = useState([]);
+    const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
 
     useEffect(() => {
         navigator.geolocation.getCurrentPosition((position) => {
@@ -41,6 +47,15 @@ export function App() {
             const place = AVAILABLE_PLACES.find((place) => place.id === id);
             return [place, ...prevPickedPlaces];
         });
+
+        const storedIds =
+            JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+        if (storedIds.indexOf(id) === -1) {
+            localStorage.setItem(
+                "selectedPlaces",
+                JSON.stringify([id, ...storedIds])
+            );
+        }
     }
 
     function handleRemovePlace() {
@@ -50,6 +65,15 @@ export function App() {
             )
         );
         modal.current.close();
+
+        const storedIds =
+            JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+        localStorage.setItem(
+            "selectedPlaces",
+            JSON.stringify(
+                storedIds.filter((id) => id !== selectedPlace.current)
+            )
+        );
     }
 
     return (
