c8439201b32b7b81b70dff23833dcea48397e043
Gabriel Cardoso Barbosa <gabrielcard0s0@hotmail.com>
2024-05-02 17:07:36 -0300

    Using multiple states for form input

--- 12-react-forms/src/components/Login.jsx
+++ 12-react-forms/src/components/Login.jsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
+
 export default function Login() {
+    const [enteredEmail, setEnteredEmail] = useState("");
+    const [enteredPassword, setEnteredPassword] = useState("");
+
     function handleSubmit(event) {
         event.preventDefault();
-        console.log("Submit")
+        console.log("Submit");
+    }
+
+    function handleEmailChange(event) {
+        setEnteredEmail(event.target.value);
+    }
+
+    function handlePasswordChange(event) {
+        setEnteredPassword(event.target.value);
     }
 
     return (
@@ -11,18 +24,34 @@ export default function Login() {
             <div className="control-row">
                 <div className="control no-margin">
                     <label htmlFor="email">Email</label>
-                    <input id="email" type="email" name="email" />
+                    <input
+                        id="email"
+                        type="email"
+                        name="email"
+                        onChange={handleEmailChange}
+                        value={enteredEmail}
+                    />
                 </div>
 
                 <div className="control no-margin">
                     <label htmlFor="password">Password</label>
-                    <input id="password" type="password" name="password" />
+                    <input
+                        id="password"
+                        type="password"
+                        name="password"
+                        onChange={handlePasswordChange}
+                        value={enteredPassword}
+                    />
                 </div>
             </div>
 
             <p className="form-actions">
-                <button type="button" className="button button-flat">Reset</button>
-                <button type="submit" className="button" onClick={handleSubmit}>Login</button>
+                <button type="button" className="button button-flat">
+                    Reset
+                </button>
+                <button type="submit" className="button" onClick={handleSubmit}>
+                    Login
+                </button>
             </p>
         </form>
     );
