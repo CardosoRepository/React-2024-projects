eab212aeb0be627f295c56c1d86ea51edcc02e81
Gabriel Cardoso Barbosa <gabrielcard0s0@hotmail.com>
2024-05-15 17:43:00 -0300

    Corrected cart length in the Header

--- 15-redux-async/src/components/Cart/CartButton.js
+++ 15-redux-async/src/components/Cart/CartButton.js
@@ -1,14 +1,15 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import classes from "./CartButton.module.css";
 import { handleShowCart } from "../../store/ui-slice";
 
 const CartButton = (props) => {
+    const cart = useSelector(state => state.cart.cartItems);
     const dispatch = useDispatch();
 
     return (
         <button className={classes.button} onClick={() => dispatch(handleShowCart())}>
             <span>My Cart</span>
-            <span className={classes.badge}>1</span>
+            <span className={classes.badge}>{cart.length}</span>
         </button>
     );
 };
