b206e9c38c9d01b0a6d1d8110e5721c1e3086a7f
Gabriel Cardoso Barbosa <gabrielcard0s0@hotmail.com>
2024-05-19 17:24:35 -0300

    P16 - bug: Removed console log inside component

--- 16-multi-page/src/pages/ProductDetail.jsx
+++ 16-multi-page/src/pages/ProductDetail.jsx
@@ -3,7 +3,6 @@ import { Link, useParams } from "react-router-dom";
 function ProductDetailPage() {
     const params = useParams();
 
-    console.log(params);
     return (
         <>
             <h1>Product Details</h1>
