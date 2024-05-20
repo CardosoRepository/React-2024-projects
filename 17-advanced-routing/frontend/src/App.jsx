93db603b753073935ce3079a603ba7ba9b8702d0
Gabriel Cardoso Barbosa <gabrielcard0s0@hotmail.com>
2024-05-20 14:18:04 -0300

    P17 - feat: Create and delete events implemented

--- 17-advanced-routing/frontend/src/App.jsx
+++ 17-advanced-routing/frontend/src/App.jsx
@@ -4,8 +4,9 @@ import HomePage from "./pages/Home";
 import EventsPage, { loader as eventsLoader } from "./pages/Events";
 import EventDetailsPage, {
     loader as eventDetailLoader,
+    action as eventDetailAction,
 } from "./pages/EventDetails";
-import NewEventPage from "./pages/NewEvent";
+import NewEventPage, { action as newEventAction } from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 import RootLayout from "./pages/RootLayout";
 import EventsRootLayout from "./pages/EventsRoot";
@@ -27,7 +28,11 @@ const router = createBrowserRouter([
                         element: <EventsPage />,
                         loader: eventsLoader,
                     },
-                    { path: "new", element: <NewEventPage /> },
+                    {
+                        path: "new",
+                        element: <NewEventPage />,
+                        action: newEventAction,
+                    },
                     {
                         path: ":eventId",
                         id: "event-detail",
@@ -36,6 +41,7 @@ const router = createBrowserRouter([
                             {
                                 index: true,
                                 element: <EventDetailsPage />,
+                                action: eventDetailAction,
                             },
                             { path: "edit", element: <EditEventPage /> },
                         ],
