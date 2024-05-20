97684dcde528245e1438a8542f2692ae70bc9c87
Gabriel Cardoso Barbosa <gabrielcard0s0@hotmail.com>
2024-05-20 10:36:50 -0300

    P17 - feat: Event detail implemented (loader used)

--- 17-advanced-routing/frontend/src/App.jsx
+++ 17-advanced-routing/frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import HomePage from "./pages/Home";
 import EventsPage, { loader as eventsLoader } from "./pages/Events";
-import EventDetailsPage from "./pages/EventDetails";
+import EventDetailsPage, { loader as eventDetailLoader } from "./pages/EventDetails";
 import NewEventPage from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 import RootLayout from "./pages/RootLayout";
@@ -26,7 +26,7 @@ const router = createBrowserRouter([
                         loader: eventsLoader,
                     },
                     { path: "new", element: <NewEventPage /> },
-                    { path: ":eventId", element: <EventDetailsPage /> },
+                    { path: ":eventId", element: <EventDetailsPage />, loader: eventDetailLoader },
                     { path: ":eventId/edit", element: <EditEventPage /> },
                 ],
             },
--- 17-advanced-routing/frontend/src/pages/EventDetails.jsx
+++ 17-advanced-routing/frontend/src/pages/EventDetails.jsx
@@ -1,10 +1,26 @@
-import { useParams } from "react-router-dom";
+import { json, useLoaderData } from "react-router-dom";
+
+import EventItem from "../components/EventItem";
 
 function EventDetailsPage() {
-    const params = useParams();
-    return (
-        <p>Event Detail Page - {params.eventId}</p>
-    )
+    const data = useLoaderData();
+
+    return <EventItem event={data.event}/>;
 }
 
 export default EventDetailsPage;
+
+export async function loader({ request, params }) {
+    const id = params.eventId;
+
+    const response = await fetch("http://localhost:8080/events/" + id);
+
+    if (!response.ok) {
+        throw json(
+            { message: "Could not fetch details for seleted event." },
+            { status: 500 }
+        );
+    }
+
+    return response;
+}
