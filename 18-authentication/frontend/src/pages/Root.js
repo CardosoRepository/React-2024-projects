ff0bdfacb1457a91839d8b53a09cd95f4e96a7ce
Gabriel Cardoso Barbosa <gabrielcard0s0@hotmail.com>
2024-05-21 17:44:37 -0300

    P18 - feat: Automatic Logout

--- 18-authentication/frontend/src/pages/Root.js
+++ 18-authentication/frontend/src/pages/Root.js
@@ -1,15 +1,33 @@
-import { Outlet, useNavigation } from "react-router-dom";
+import { Outlet, useLoaderData, useSubmit } from "react-router-dom";
 
 import MainNavigation from "../components/MainNavigation";
+import { useEffect } from "react";
+import { getTokenDuration } from "../util/auth";
 
 function RootLayout() {
-    // const navigation = useNavigation();
+    const token = useLoaderData();
+    const submit = useSubmit();
+
+    useEffect(() => {
+        if (!token) {
+            return;
+        }
+
+        if (token === "EXPIRED_TOKEN") {
+            submit(null, { action: "/logout", method: "post" });
+        }
+
+        const tokenDuration = getTokenDuration();
+
+        setTimeout(() => {
+            submit(null, { action: "/logout", method: "post" }); // No value is passed and /logou form is submitted
+        }, tokenDuration);
+    }, [token, submit]);
 
     return (
         <>
             <MainNavigation />
             <main>
-                {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
                 <Outlet />
             </main>
         </>
