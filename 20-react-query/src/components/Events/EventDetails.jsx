0654ae50852262649ea786ae56a94b7d4c50be9a
Cardoso <gabrielcard0s0@hotmail.com>
2024-12-09 15:19:45 -0300

    feat: delete functionality implemented

--- 20-react-query/src/components/Events/EventDetails.jsx
+++ 20-react-query/src/components/Events/EventDetails.jsx
@@ -1,19 +1,37 @@
-import { Link, Outlet, useParams } from "react-router-dom";
+import {Link, Outlet, useNavigate, useParams} from "react-router-dom";
 
 import Header from "../Header.jsx";
-import { useQuery } from "@tanstack/react-query";
-import { fetchEvent } from "../../util/http.js";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import {deleteEvent, fetchEvent, queryClient} from "../../util/http.js";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function EventDetails() {
     const { id } = useParams();
+    const navigate = useNavigate();
     const { data, isPending, isError, error } = useQuery({
         queryKey: ["events", { id }],
         queryFn: ({ signal }) => fetchEvent({ id, signal }),
     });
 
+    const { mutate } = useMutation({
+        mutationFn: deleteEvent,
+        onSuccess: () => {
+            queryClient.invalidateQueries(({
+                queryKey: ['events']
+            }))
+            navigate("/events")
+        }
+    })
+
+    function handleDelete() {
+        mutate({ id: id });
+    }
+
     let content;
 
+    // const { mutate } = useMutation({
+    //     mutationFn: deleteEvent
+    // })
     if (isPending) {
         content = (
             <div id="event-details-content" className="center">
@@ -48,7 +66,7 @@ export default function EventDetails() {
                 <header>
                     <h1>{data.title}</h1>
                     <nav>
-                        <button>Delete</button>
+                        <button onClick={handleDelete}>Delete</button>
                         <Link to="edit">Edit</Link>
                     </nav>
                 </header>
