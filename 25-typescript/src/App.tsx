8980dac4a83bd8551c34adcd5882bb6abedaa582
Cardoso <gabrielcard0s0@hotmail.com>
2025-04-06 23:31:22 -0300

    feat (25-typescript): implementing model class for Todos

--- 25-typescript/src/App.tsx
+++ 25-typescript/src/App.tsx
@@ -1,9 +1,16 @@
 import Todos from "./components/Todos";
+import Todo from "./models/todo";
 
 function App() {
+
+    const todos: Todo[] = [
+        new Todo('Learn React'),
+        new Todo('Learn TypeScript'),
+    ];
+
     return (
         <div>
-            <Todos items={['a', 'b']} />
+            <Todos items={todos} />
         </div>
     )
 }
